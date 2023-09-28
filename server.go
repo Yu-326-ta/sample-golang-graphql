18bbbd27d2cfdb6c029b1aa516d74f9a7ac77397
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-09-28 21:24:22 +0900

    create user resolver

--- server.go
+++ server.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"graphql_sample/graph"
+	"graphql_sample/graph/services"
 	"graphql_sample/internal"
 	"log"
 	"net/http"
@@ -11,15 +14,28 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-const defaultPort = "8080"
+const (
+dbFile      = "./mygraphql.db"
+defaultPort = "8080"
 
+)
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(internal.NewExecutableSchema(internal.Config{Resolvers: &graph.Resolver{}}))
+	db, err := sql.Open("sqlite3", fmt.Sprintf("%s?_foreign_keys=on", dbFile))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	service := services.New(db)
+
+	srv := handler.NewDefaultServer(internal.NewExecutableSchema(internal.Config{Resolvers: &graph.Resolver{
+		Srv:     service,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
