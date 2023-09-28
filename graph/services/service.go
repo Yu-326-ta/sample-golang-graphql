cb6ed459cd5d0cb37fe554215f9e1b31e351e9cc
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-09-28 21:53:55 +0900

    change sqlboiler version and regenerate db folder

--- graph/services/service.go
+++ graph/services/service.go
@@ -13,10 +13,12 @@ type UserService interface {
 
 type Services interface {
 	UserService
+	// issueテーブルを扱うIssueServiceなど、他のサービスインターフェースができたらそれらを追加していく
 }
 
 type services struct {
 	*userService
+	// issueテーブルを扱うissueServiceなど、他のサービス構造体ができたらフィールドを追加していく
 }
 
 func New(exec boil.ContextExecutor) Services {
